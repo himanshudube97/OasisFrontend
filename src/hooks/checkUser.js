b18016f2949ee3edfd9d790b6d4d9897c278871e
himanshudube97 <himanshu.dube13@gmail.com>
2024-01-14 00:00:19 +0530

    fixed the loaidng issue , have to learn more

--- src/hooks/checkUser.js
+++ src/hooks/checkUser.js
@@ -3,7 +3,7 @@ import { useAuthStore, useChatStore } from "../Zustand/store";
 import { jwtDecode } from "jwt-decode";
 
 export const useCheckUser = () => {
-    const [loading, setLoading] = useState(false);
+    const [loading, setLoading] = useState(true);
     const token = localStorage.getItem("token");
 
     // eslint-disable-next-line no-unused-vars
@@ -12,22 +12,24 @@ export const useCheckUser = () => {
     });
     const { chats, getChats } = useChatStore((state) => {
         return { ...state };
-      });
+    });
 
     useEffect(() => {
-        async function getUserInfo(userId){
+        async function getUserInfo(userId) {
             setLoading(true);
-              await getSingleUser(userId)
-              await getChats(userId);
+            await getSingleUser(userId)
+            await getChats(userId);
             setLoading(false);
         }
         if (token) {
             const decoded = jwtDecode(token);
-             getUserInfo(decoded.id);
+            getUserInfo(decoded.id);
         }
     }, [token, getSingleUser, getChats])
 
+    if (!token) {
+        return { loading: false, userData, chats }
+    }
 
-    
-    return {loading,userData, chats}
+    return { loading, userData, chats }
 }
