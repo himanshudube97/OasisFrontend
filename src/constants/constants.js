7ef1c981881a0fb3da4f6bb07653ad3af4061626
himanshudube97 <himanshu.dube13@gmail.com>
2024-09-18 23:28:46 +0530

    addded new constants

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -4,3 +4,10 @@ export const UserFollowersList = {
         FOLLOWERS: "MY_FOLLOWERS",
         NOT_FOLLOWED: "NOT_FOLLOWED"
 }
+
+export const SavedStatus ={
+        SAVED :"SAVE",
+        OVERWRITE: "OVERWRITE",
+        UNSAVED_CHANGES: "UNSAVED_CHANGES",
+        DELETE: "DELETE_CHANGES"
+}
