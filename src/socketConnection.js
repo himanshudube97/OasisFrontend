a2bb403bd571d9f8d03b243fa5dadbf48f3480b2
himanshudube97 <himanshu.dube13@gmail.com>
2024-10-04 22:04:09 +0530

    ttes1

--- src/socketConnection.js
+++ src/socketConnection.js
@@ -3,3 +3,11 @@ export const socketConnectFunction = () => {
   const socket = io("http://localhost:4000", {autoConnect: false});
   return socket;
 };
+
+
+//Ignore all the below ones. Testing husky
+
+
+const obj={
+  hello: "Hello  "   ,
+}
